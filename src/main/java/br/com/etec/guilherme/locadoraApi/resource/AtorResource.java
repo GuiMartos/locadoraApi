6fff7146ace3456e3fb461730cd72e609e1ac050
GuiMartos <guilhermemartos2005@gmail.com>
2023-04-19 08:56:44 -0300

    Terminando o "Resource"

--- src/main/java/br/com/etec/guilherme/locadoraApi/resource/AtorResource.java
+++ src/main/java/br/com/etec/guilherme/locadoraApi/resource/AtorResource.java
@@ -1,4 +1,25 @@
 package br.com.etec.guilherme.locadoraApi.resource;
 
+
+import br.com.etec.guilherme.locadoraApi.model.Ator;
+import br.com.etec.guilherme.locadoraApi.repository.AtorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/atores")
 public class AtorResource {
+
+    @Autowired
+    private AtorRepository atorRepository;
+
+    @GetMapping("/todos")
+    public List<Ator> listarTodosAtores(){
+        return atorRepository.findAll(Sort.by("nomeator").ascending());
+    }
 }
--- src/main/java/br/com/etec/guilherme/locadoraApi/resource/FilmeResource.java
+++ src/main/java/br/com/etec/guilherme/locadoraApi/resource/FilmeResource.java
@@ -1,4 +1,24 @@
 package br.com.etec.guilherme.locadoraApi.resource;
 
+import br.com.etec.guilherme.locadoraApi.model.Filme;
+import br.com.etec.guilherme.locadoraApi.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/filmes")
 public class FilmeResource {
+
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+    @GetMapping("/todos")
+    public List<Filme> listarTodosFilmes(){
+        return filmeRepository.findAll(Sort.by("nomefilme").ascending());
+    }
 }
