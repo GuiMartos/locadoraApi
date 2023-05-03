cb47a1a326944be151767ad10fb4fb4aaebd93b2
GuiMartos <guilhermemartos2005@gmail.com>
2023-05-03 08:32:36 -0300

    Update GeneroFilter.java

--- src/main/java/br/com/etec/guilherme/locadoraApi/repository/filter/GeneroFilter.java
+++ src/main/java/br/com/etec/guilherme/locadoraApi/repository/filter/GeneroFilter.java
@@ -3,8 +3,6 @@ package br.com.etec.guilherme.locadoraApi.repository.filter;
 public class GeneroFilter {
 
     private String descricao;
-    private String nomeator;
-    private String nomegenero;
 
     public String getDescricao() {
         return descricao;
